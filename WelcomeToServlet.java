2e67a9582de283c0cb1fac4da405c5defa1492af
Gopal Krishna Jha <141767531+gopal-krishna-jha@users.noreply.github.com>
2025-02-22 10:33:26 +0530

    [Gopal Krishna Jha] Add Welcome File

---
+++ WelcomeToServlet.java
@@ -0,0 +1,7 @@
+//Create a WelcomeToServlet to print the welcome statement
+public class WelcomeToServlet {
+    public static void main(String[] args) {
+        System.out.println("\"Welcome to the Servlet!\"");
+    }
+
+}
